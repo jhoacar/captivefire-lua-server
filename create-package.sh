3f69abd14e18155db1b8370e8f5825d406756f9b
jhoacar <carrerojhoan@gmail.com>
2022-08-20 20:33:54 -0300

    added script for create package and correct control

--- create-package.sh
+++ create-package.sh
@@ -1,10 +1,20 @@
-#!/bin/bash
+#!/bin/sh
 
 set -eux
 
-BUILDED=/home/build/openwrt/bin/packages
-IMAGE=captivefire:package-builder
+FOLDER=$(pwd)/ipk
+OUTPUT=$(pwd)/build/captivefire-0.0.1.ipk
 
-docker build -f packages/Dockerfile -t $IMAGE packages
+rm -rf $OUTPUT
 
-docker run --rm -it -v $(pwd)/packages/build:$BUILDED $IMAGE make package/captivefire/compile
+cd $FOLDER/control
+tar --numeric-owner --group=0 --owner=0 -czvf ../control.tar.gz ./*
+
+cd $FOLDER/data
+tar --numeric-owner --group=0 --owner=0 -czvf ../data.tar.gz ./
+
+cd $FOLDER
+echo 2.0 > debian-binary
+tar --numeric-owner --group=0 --owner=0 -czvf $OUTPUT debian-binary control.tar.gz data.tar.gz  
+
+rm -rf control.tar.gz data.tar.gz debian-binary
