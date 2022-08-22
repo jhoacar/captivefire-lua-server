ca6bbd82695e1d3b72faf484dd49558a99c1d92c
jhoacar <carrerojhoan@gmail.com>
2022-08-22 02:23:35 -0300

    added correct debug option in docker entrypoint

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 
-echo "" > /app/debug.txt
-cp /app/docker/etc/config/uhttpd /app  >> /app/debug.txt 2>&1
-rm /app/public/cgi-bin /app/public/luci-static  >> /app/debug.txt 2>&1
-opkg update  >> /app/debug.txt 2>&1
-opkg install coreutils-chown >> /app/debug.txt 2>&1
-opkg install /app/package/captivefire-0.0.1.ipk  >> /app/debug.txt 2>&1
-chown -R 1000:1000 /app >> /app/debug.txt 2>&1
-echo "done" >> /app/debug.txt
+set -x
+
+logread -f > /app/debug.txt &
+
+cp /app/docker/etc/config/uhttpd /app 
+rm /app/public/cgi-bin /app/public/luci-static 
+opkg update 
+opkg install coreutils-chown
+opkg install /app/package/captivefire-0.0.1.ipk 
+chown -R 1000:1000 /app
+echo "done"
