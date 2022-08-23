dbb1929a07466cfe38738f14c0b7d349ab58519e
jhoacar <carrerojhoan@gmail.com>
2022-08-22 22:36:47 -0300

    removed eval function in script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -6,6 +6,7 @@ logread -f | strings > /app/debug.txt &
 
 opkg update 
 opkg install coreutils-chown
+echo "" > /etc/config/wireless
 opkg install /app/package/captivefire-0.0.1.ipk 
 chown -R 1000:1000 /app
 echo "done"
